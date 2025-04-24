0404c627bd01c87373c6b704d12ef1aff8cd5ce1
Yogesh Khatri <khatriyoga@gmail.com>
2025-04-24 23:50:24 +0530

    add boilerplate functions for user

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,13 @@
+const registerUser = (req, res) => {};
+
+const verifyUser = (req, res) => {};
+
+const login = (req, res) => {};
+
+const updateUser = (req, res) => {};
+
+const refreshToken = (req, res) => {};
+
+const passwordResetToken = (req, res) => {};
+
+const accessToken = (req, res) => {};
